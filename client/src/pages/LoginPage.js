6e20a717bee232f92b37fd47ea3e152580a2dbd2
DaniellaKarlen <daniella.karlen@gmail.com>
2023-05-25 17:05:36 +0200

    up next login

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
+
+const LOGIN_URL = "http://localhost:4000/auth/login";
+
 
 
 export const LoginPage = () => {
+  
+  
 
   return (
     <div className='login-container'>
@@ -9,9 +15,9 @@ export const LoginPage = () => {
         <div className='login-wrapper'>
             <form className='login-form'>
                 <label>Username</label>
-                <input type="text" placeholder='Type your username...'></input>
+                <input type="text" placeholder='Type your username...' autoComplete='off' required></input>
                 <label>Password</label>
-                <input type="password" placeholder='Type your password...'></input>
+                <input type="password" placeholder='Type your password...' required></input>
             </form>
             <p>No account? Sign up <a href="/register" >here!</a></p>
             <button type="submit">Sign in</button>
@@ -20,3 +26,7 @@ export const LoginPage = () => {
     </div>
   )
 }
+
+
+// form onSubmit={handleSubmit}
+// button innanför form för submit.
